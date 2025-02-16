658228cd776d81e7a3148e795216aea00f28af1d
aymanhaque <muhd.ayman.haque@gmail.com>
2025-02-16 17:40:20 -0600

    done task 7

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, abort
 from EmotionDetection import emotion_detection
 app = Flask(__name__)
 
@@ -9,6 +9,8 @@ def hello_world():
 @app.route('/emotionDetector')
 def emotionOutput():
     response = request.args.get('textToAnalyze')
+    if response =="":
+        abort(400)
     emotions = emotion_detection.emotion_detector(response)
     statement = f"""For the given statement, the system response is 
         'anger': {emotions['anger']}, 
@@ -20,3 +22,7 @@ def emotionOutput():
         The dominant emotion is {emotions['dominant_emotion']}.
         """
     return statement
+
+@app.errorhandler(400)
+def bad_request(e):
+    return "Invalid request. Please check your input.", 400
