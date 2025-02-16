be374e310f4f4ff0d5afd41d8b87d1296fef2538
aymanhaque <muhd.ayman.haque@gmail.com>
2025-02-15 23:52:57 -0500

    Finished Task 3

---
+++ emotion_detection.py
@@ -0,0 +1,16 @@
+import requests, json
+
+def emotion_detector(text_to_analyse):
+    URL = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    Headers= {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    Input = { "raw_document": { "text": text_to_analyse } }
+
+    response = requests.post(URL, json = Input, headers = Headers)
+
+    dictionary= json.loads(response.text)['emotionPredictions'][0]['emotion']
+    dominant = max(dictionary, key = dictionary.get)
+    dictionary['domiant_emotion'] = dominant
+
+    return dictionary
+
+print(emotion_detector('I am so happy I am doing this'))
