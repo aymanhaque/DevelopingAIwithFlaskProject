e5f6ece1b8eb01d2a5d69eedb68cfb70cae253b1
aymanhaque <muhd.ayman.haque@gmail.com>
2025-02-16 10:44:43 -0500

    made server.py

--- server.py
+++ server.py
@@ -1,1 +1,11 @@
-from flask import Flask
+# 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
+
+from flask import Flask, request
+from EmotionDetection import emotion_detection
+app = Flask(__name__)
+
+@app.route('/emotionDetector')
+def emotionOutput():
+    response = request.args.get('textToAnalyze')
+    emotions = emotion_detector(response)
+    return emotions
