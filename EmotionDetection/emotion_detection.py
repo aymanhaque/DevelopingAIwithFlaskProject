a82d90b9ad2571a00d3bf60dc6b286b62445c13c
aymanhaque <muhd.ayman.haque@gmail.com>
2025-02-16 00:31:05 -0500

    Done with task 3. on task 4

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -6,11 +6,11 @@ def emotion_detector(text_to_analyse):
     Input = { "raw_document": { "text": text_to_analyse } }
 
     response = requests.post(URL, json = Input, headers = Headers)
-
     dictionary= json.loads(response.text)['emotionPredictions'][0]['emotion']
     dominant = max(dictionary, key = dictionary.get)
     dictionary['domiant_emotion'] = dominant
+    
 
     return dictionary
 
-print(emotion_detector('I am so happy I am doing this'))
+print(emotion_detector(' I am so happy I am doing this'))
